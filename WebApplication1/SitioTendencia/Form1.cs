f00e34a15a91ff3bd75f34a45c668682d5c477f3
CristianFGC12 <90874820+CristianFGC12@users.noreply.github.com>
2024-05-10 16:41:59 -0600

    Add files via upload

---
+++ WebApplication1/SitioTendencia/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SitioTendencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Usuario = textBox1.Text;
+            string Pagina = textBox2.Text;
+            var cliente = new ServiceReference1.LabServiceSoapClient();
+            bool permisos = cliente.PedirPermiso(Usuario, Pagina);
+            if (permisos == true)
+            {
+                label4.Text = "Si posee permisos";
+            }
+            else 
+            {
+                label4.Text = "No posee permisos";
+            }
+        }
+    }
+}
